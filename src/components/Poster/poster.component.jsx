fae4a17914011f6c1996d72b5f1287c0b5e4ba6d
PatilDhanashri <dhanashrip7878@gmail.com>
2021-07-25 19:45:44 +0530

    added Cast & crew

--- src/components/Poster/poster.component.jsx
+++ src/components/Poster/poster.component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+
 const Poster = (props) => {
   return (
-    <Link to ={`/movie/${props.id}`}>
+    <Link to={`/movie/${props.id}`}>
       <div className="flex flex-col items-start gap-2 px-1 md:px-3">
         <div className="h-40 md:h-80">
           <img
-             src={`https://image.tmdb.org/t/p/original${props.poster_path}`}
+            src={`https://image.tmdb.org/t/p/original${props.poster_path}`}
             alt={props.original_title}
             className="w-full h-full rounded-md"
           />
@@ -22,7 +23,7 @@ const Poster = (props) => {
           className={`text-sm ${props.isDark ? "text-white" : "text-gray-700"}`}
         >
           {props.subtitle}
-        </p> 
+        </p>
       </div>
     </Link>
   );
--- src/components/PosterSlider/PosterSlider.component.jsx
+++ src/components/PosterSlider/PosterSlider.component.jsx
@@ -1,34 +1,39 @@
-import React from 'react';
-//components
-//slider
+import React from "react";
 import Slider from "react-slick";
+// components
+import Poster from "../Poster/poster.component";
 
-import Poster from "../../Poster/poster.component";
-//Configs
+// Configs
 import Postercarousalsettins from "../../Config/PosterCarousel.config";
-export const PosterSlider = (props) => {
 
-  const sliderConfig = props.config ? props.config :Postercarousalsettins;
-    return (
-        <>
-          <div className="flex flex-col items-start my-2">
-            <h3
-            className={` text-2xl font-bold ${
-                props.isDark ? "text-white" : "text-gray-800" }`}>
-            {props.title}</h3>
+const PosterSlider = (props) => {
+  const sliderConfig = props.config ? props.config : Postercarousalsettins;
 
-            <p
-            className={` text-sm ${
-                props.isDark ? "text-white" : "text-gray-800"}`}>
-            {props.Subtitle}</p>
-       </div>
-
+  return (
+    <>
+      <div className="flex flex-col items-start my-2">
+        <h3
+          className={` text-2xl font-bold ${
+            props.isDark ? "text-white" : "text-gray-800"
+          }`}
+        >
+          {props.title}
+        </h3>
+        <p
+          className={` text-sm ${
+            props.isDark ? "text-white" : "text-gray-800"
+          }`}
+        >
+          {props.subtitle}
+        </p>
+      </div>
       <Slider {...sliderConfig}>
         {props.images.map((image) => (
-          <Poster {...image} isDark={props.isDark}/>
+          <Poster {...image} isDark={props.isDark} />
         ))}
-      </Slider>  
-        </>
-    )
-}
+      </Slider>
+    </>
+  );
+};
+
 export default PosterSlider;
